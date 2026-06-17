28663e92bd7f08aeb801ba0b3de4684019e3cde1
Ujwal Mamidi <uji.mamidi@gmail.com>
2026-06-18 00:29:52 +1000

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+    const currentDate = new Date().toDateString();
+
+    return (
+        <div className="flex flex-row items-center justify-between mx-150 mt-5">
+            <h1 className="font-bbh text-lg">PANDHI</h1>
+            <p className="text-sm italic">{currentDate}</p>
+        </div>
+    )
+}
+
+export default Navbar;
